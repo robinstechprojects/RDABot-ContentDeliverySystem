07cf2df0be0ff7f2ed23343c80f9e567b51644dc
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-26 12:08:34 +0200

    Add Message on Startup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import socket
 
+print("RDA - ContenDeliverySystem started !")
+print("! Please Visit www.sunrobindev.de !")
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create socket
 server_socket.bind((socket.gethostname(), 1337)) #Bind socket to IP and port
 server_socket.listen(1) #Backlog (number) defines max. clients
