d9ef83e9d725a07c29fe1371889d6d5e2ed198f4
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-26 12:05:46 +0200

    Build the socket

---
+++ main.py
@@ -0,0 +1,9 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create socket
+server_socket.bind((socket.gethostname(), 1337)) #Bind socket to IP and port
+server_socket.listen(1) #Backlog (number) defines max. clients
+while True:
+    (client_socket, addr) = server_socket.accept() #accept connection
+    msg = client_socket.recv(1024)
+    print(str(msg, "utf8")) #recive from client
