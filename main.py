56b3135825ef478205d16779e41d78db9c2f9504
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-28 10:45:40 +0200

    multithreading to handle multiple connections

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import socket
+import threading
 
 print("RDA - ContenDeliverySystem started !")
 print("! Please Visit www.sunrobindev.de !")
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create socket
-server_socket.bind((socket.gethostname(), 1337)) #Bind socket to IP and port
-server_socket.listen(1) #Backlog (number) defines max. clients
+server_socket.bind(('127.0.0.1', 1338)) #Bind socket to IP and port
+server_socket.listen(2) #Backlog (number) defines max. clients
+
+class Fred(threading.Thread):
+    def __init__(self, iD, client_socket):
+        threading.Thread.__init__(self)
+        self.iD = iD
+        self.client_socket = client_socket
+
+    def run(self):
+        while True:
+            msg = client_socket.recv(1024)
+            print(str(msg, "utf8")) #recive from client
+
+
 while True:
     (client_socket, addr) = server_socket.accept() #accept connection
-    msg = client_socket.recv(1024)
-    print(str(msg, "utf8")) #recive from client
+    ct1 = Fred(1, client_socket)
+    ct1.start()
