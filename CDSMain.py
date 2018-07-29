a4ae2b77034ada29753da81e573e7aa4e04601a0
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-29 11:50:36 +0200

    Update IP

--- CDSMain.py
+++ CDSMain.py
@@ -1,7 +1,7 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create socket
-server_socket.bind(('127.0.0.1', 1338)) #Bind socket to IP and port
+server_socket.bind(('185.249.199.49', 1338)) #Bind socket to IP and port
 server_socket.listen(1) #Backlog (number) defines max. clients
 while True:
     (client_socket, addr) = server_socket.accept()#accept Connction
