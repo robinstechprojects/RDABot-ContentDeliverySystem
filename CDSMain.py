f97802932d00b2242f3109a3ce4ae6197a4cd23c
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-07-29 18:35:14 +0200

    metal songs

--- CDSMain.py
+++ CDSMain.py
@@ -31,3 +31,10 @@ while True:
         title = random.choice(song)
         print("suggested: " + title)
         client_socket.send(bytes(title, "utf8"))
+    elif str(msg, "utf8") == "metalsong":
+        file = open('metalsongs.txt',mode='r')
+        song = file.readlines()
+        file.close()
+        title = random.choice(song)
+        print("suggested: " + title)
+        client_socket.send(bytes(title, "utf8"))
