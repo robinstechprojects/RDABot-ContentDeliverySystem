2b9e21e4c335099342a2f5bdee8c9bdae6a89b15
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-08-14 19:59:56 +0200

    remove IP

--- CDSMain.py
+++ CDSMain.py
@@ -6,7 +6,7 @@ import requests
 print("RDA-Bot ContentDeliverySystem Started")
 print("! Please visit www.sunrobindev.de !")
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create socket
-server_socket.bind(('185.249.199.220', 1338)) #Bind socket to IP and port
+server_socket.bind(('127.0.0.1', 1338)) #Bind socket to IP and port
 server_socket.listen(1) #Backlog (number) defines max. clients
 while True:
     (client_socket, addr) = server_socket.accept()#accept Connction
